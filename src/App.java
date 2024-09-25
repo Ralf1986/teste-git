5f929ceb8c6cc09578ef0ae9cc815ed8a5d1358f
Ralf1986 <ralfsantos.oliveira@gmail.com>
2024-09-24 21:20:27 -0300

    concatenação de nome

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+
+        String primeiroNome = "Ralf";
+        String segundoNome = "Santos de Oliveira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
